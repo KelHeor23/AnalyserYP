8a988d8cb2d283cf2475b9c2fef186eb54402bde
KelHeor23 <kelheor23@gmail.com>
2025-12-03 03:55:38 +0300

    MetricExtractor

--- src/metric.cpp
+++ src/metric.cpp
@@ -30,8 +30,17 @@ void MetricExtractor::RegisterMetric(std::unique_ptr<IMetric> metric) { metrics.
  * к переданной функции `func` и собирает результаты в вектор.
  */
 MetricResults MetricExtractor::Get(const function::Function &func) const {
-    // здесь ваш код
-    return {};
+    MetricResults results;
+    results.reserve(metrics.size());
+
+    for (const auto &metric : metrics) {
+        if (!metric) {
+            continue;  
+        }
+        results.push_back(metric->Calculate(func));
+    }
+
+    return results;
 }
 
 }  // namespace analyzer::metric
