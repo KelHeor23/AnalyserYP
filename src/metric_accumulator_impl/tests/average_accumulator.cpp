2dc48342bfc434a2c352c8070117d54c972909a2
KelHeor23 <kelheor23@gmail.com>
2025-12-04 03:24:23 +0300

    accumulator tests

--- src/metric_accumulator_impl/tests/average_accumulator.cpp
+++ src/metric_accumulator_impl/tests/average_accumulator.cpp
@@ -6,7 +6,59 @@
 
 namespace analyzer::metric_accumulator::metric_accumulator_impl::test {
 
-// здесь ваш код
-TEST(BasicCheck, Sum) { EXPECT_EQ(1 + 1, 2); }
+TEST(AverageAccumulatorTest, BasicAverage) {
+    AverageAccumulator accumulator;
+
+    accumulator.Accumulate(metric::MetricResult{"test", 1});
+    accumulator.Accumulate(metric::MetricResult{"test", 2});
+    accumulator.Accumulate(metric::MetricResult{"test", 3});
+    accumulator.Accumulate(metric::MetricResult{"test", 4});
+    accumulator.Accumulate(metric::MetricResult{"test", 5});
+    accumulator.Accumulate(metric::MetricResult{"test", 6});
+    accumulator.Accumulate(metric::MetricResult{"test", 7});
+
+    accumulator.Finalize();
+
+    EXPECT_DOUBLE_EQ(accumulator.Get(), 4.);
+}
+
+TEST(AverageAccumulatorTest, Reset) {
+    AverageAccumulator accumulator;
+
+    accumulator.Accumulate(metric::MetricResult{"test", 1});
+    accumulator.Accumulate(metric::MetricResult{"test", 2});
+    accumulator.Accumulate(metric::MetricResult{"test", 3});
+    accumulator.Finalize();
+
+    EXPECT_DOUBLE_EQ(accumulator.Get(), 2.);
+
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{"test", 1});
+    accumulator.Accumulate(metric::MetricResult{"test", 2});
+    accumulator.Accumulate(metric::MetricResult{"test", 3});
+    accumulator.Accumulate(metric::MetricResult{"test", 4});
+    accumulator.Accumulate(metric::MetricResult{"test", 5});
+    accumulator.Accumulate(metric::MetricResult{"test", 6});
+    accumulator.Accumulate(metric::MetricResult{"test", 7});
+    accumulator.Finalize();
+
+    EXPECT_DOUBLE_EQ(accumulator.Get(), 4.);
+}
+
+TEST(AverageAccumulatorTest, WithoutFinalize) {
+    AverageAccumulator accumulator;
+
+    accumulator.Accumulate(metric::MetricResult{"test", 1});
+
+    EXPECT_THROW(accumulator.Get(), std::runtime_error);
+}
+
+TEST(AverageAccumulatorTest, OnlyFinalize) {
+    AverageAccumulator accumulator;
+
+    accumulator.Finalize();
+
+    EXPECT_NO_THROW(accumulator.Get());
+}
 
 }  // namespace analyzer::metric_accumulator::metric_accumulator_impl::test
